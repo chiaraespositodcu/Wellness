ba33c95349741d72b1adfecbfb49cbc3e9c3cebc
chiaraespositodcu <esposito.chiara2@mail.dcu.ie>
2022-11-27 18:18:38 +0000

    Tidy html

--- Contact/request.php
+++ Contact/request.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<body>
-
-Welcome <?php echo $_POST["firstname"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
 Thank you for your request!
 We have received your enquiry and will get in contact with you soon.
-
-<a href="../index.html">Back to homepage</a>
-
-</body>
-</html>
